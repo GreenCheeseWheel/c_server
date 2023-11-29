136e5243b8d527a8f7d21c4a64543108048000e5
GreenCheeseWheel <thegreencheesewheel@gmail.com>
2023-11-29 19:49:43 -0300

    Server now functioning better

--- server/server.c
+++ server/server.c
@@ -2,7 +2,10 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+
+#define MAX_CON 5
 
 int main(int argc, char *argv[]) {
     if(argc < 2) {
@@ -19,10 +22,10 @@ int main(int argc, char *argv[]) {
 
     struct sockaddr_in addr = {
         .sin_family = AF_INET,
-        .sin_port = htons(3500)
+        .sin_port = htons(atoi(argv[1]))
     };
 
-    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     int bind_res = bind(sckt, (struct sockaddr*)&addr, sizeof(addr));
 
@@ -31,12 +34,16 @@ int main(int argc, char *argv[]) {
         exit(-1);
     }
 
-    listen(sckt, 1);
+    listen(sckt, MAX_CON);
+
+    char *message = "HTTP/1.0\r\nContent-Type: text/html\r\n\r\n<p>Este mensaje es un <b>mensaje en vano</b></p>\0";
+    char sckt_msg[5];
 
     while (1)
     {   
         struct sockaddr_in client_addr;
         socklen_t client_size = sizeof(client_addr);
+        memset(&client_addr, 0, client_size);
 
         int conn_sckt = accept(sckt, (struct sockaddr*)&client_addr, &client_size);
 
@@ -45,8 +52,19 @@ int main(int argc, char *argv[]) {
             exit(-1);
         }
 
+        char clntName[INET_ADDRSTRLEN];
+
+        if(inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, clntName, sizeof(clntName)) != NULL) {
+            printf("Handling client %s/%d\n", clntName, ntohs(client_addr.sin_port));
+        }
+
+        int send_status = send(conn_sckt, message, strlen(message), 0);
+
+        if(send_status < 0) {
+            fputs("Error sending socket info.", stderr);
+        }
+
         close(conn_sckt);
-        break;
     }   
     
 
